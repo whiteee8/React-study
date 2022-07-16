9c192261d592fa4af1338f310596a130910f754b
eunmi <eunmi208@konkuk.ac.kr>
2022-07-16 23:14:19 +0900

    [react-for-beginners] 6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    console.log("i run all the time"); //state가 바뀔때마다 항상 실행
+function Hello() {
     useEffect(() => {
-        console.log("i run only once"); //state가 바뀌어도 한번만 실행
+        console.log("created"); //Hello 컴포넌트가 등장할때마다 새로 render
+        return () => console.log("destroyed"); //Hello컴포넌트가 사라질때 실행되는 함수.
+        //리턴에 함수를 넣어줌
     }, []);
-    useEffect(() => {
-        /*if (keyword !== "" && keyword.length > 5)
-            console.log("search for", keyword);*/
-        //두번째 인자인 keyword state가 바뀔때만 실행
-        console.log("i run when 'keyword' changes");
-    }, [keyword]);
-    //console.log("search for", keyword); //counter가 바뀔때도 계속 실행되는 문제, keyword의 변화에만 실행돼야함
-    useEffect(() => {
-        console.log("i run when 'counter' changes");
-    }, [counter]);
-    useEffect(() => {
-        console.log("i run when 'keyword' & 'counter' changes");
-    }, [keyword, counter]);
+    return <h1>Hello</h1>;
+}
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder="Search here..."
-            />
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
