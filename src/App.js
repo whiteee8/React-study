a3ec4a9e160fbaa73c320a307d731c17972d4ec8
eunmi <eunmi208@konkuk.ac.kr>
2022-07-16 22:20:27 +0900

    [react-for-beginners] 6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("call an api"); //state가 바뀔때마다 이 콘솔이 계속 찍히는것처럼 이 부분에 어떤 코드가 들어오든 항상 실행됨
-    //처음 render할 떄 한번만 실행하고 그뒤론 다시 실행하지 않는 방법 필요
+    //처음 render할 떄 한번만 실행하고 그뒤론 다시 실행하지 않는 방법 필요 -> useEffect
+    console.log("i run all the time"); //state가 바뀔때마다 항상 실행
+    useEffect(() => {
+        console.log("call the api ..."); //state가 바뀌어도 한번만 실행
+    }, []);
     return (
         <div>
             <h1>{counter}</h1>
